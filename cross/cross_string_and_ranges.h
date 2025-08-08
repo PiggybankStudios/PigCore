94759995dd07b927cde53a16c87de717ccfe4fcc
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2025-08-08 14:11:01 -0700

    Added more things to cross_string_and_ranges.h (StrRange which is a combination of Str8 and RangeUXX and related functions)

--- cross/cross_string_and_ranges.h
+++ cross/cross_string_and_ranges.h
@@ -9,10 +9,27 @@ Date:   08\08\2025
 
 //NOTE: Intentionally no includes here
 
+typedef plex StrRange StrRange;
+plex StrRange
+{
+	Str8 str;
+	RangeUXX range;
+};
+
+// +--------------------------------------------------------------+
+// |                 Header Function Declarations                 |
+// +--------------------------------------------------------------+
 #if !PIG_CORE_IMPLEMENTATION
 	PIG_CORE_INLINE RangeUXX SliceToRangeUXX(Str8 str, Str8 slice);
+	PIG_CORE_INLINE Str8 StrSliceRange(Str8 str, RangeUXX range);
+	PIG_CORE_INLINE StrRange NewStrRange(Str8 str, RangeUXX range);
+	PIG_CORE_INLINE Str8 ToStr8FromRange(StrRange strRange);
+	PIG_CORE_INLINE StrRange ToStrRange(Str8 str, Str8 slice);
 #endif
 
+// +--------------------------------------------------------------+
+// |                   Function Implementations                   |
+// +--------------------------------------------------------------+
 #if PIG_CORE_IMPLEMENTATION
 
 PEXPI RangeUXX SliceToRangeUXX(Str8 str, Str8 slice)
@@ -22,6 +39,26 @@ PEXPI RangeUXX SliceToRangeUXX(Str8 str, Str8 slice)
 	NotNull(str.chars);
 	Assert(IsSizedPntrWithin(str.chars, str.length, slice.chars, slice.length));
 	return NewRangeUXX((uxx)(slice.chars - str.chars), (uxx)(slice.chars - str.chars) + slice.length);
+}
+
+PEXPI Str8 StrSliceRange(Str8 str, RangeUXX range) { return StrSlice(str, range.min, range.max); }
+
+PEXPI StrRange NewStrRange(Str8 str, RangeUXX range)
+{
+	StrRange result = ZEROED;
+	result.str = str;
+	result.range = range;
+	return result;
+}
+
+PEXPI Str8 ToStr8FromRange(StrRange strRange)
+{
+	return StrSliceRange(strRange.str, strRange.range);
+}
+
+PEXPI StrRange ToStrRange(Str8 str, Str8 slice)
+{
+	return NewStrRange(str, SliceToRangeUXX(str, slice));
 }
 
 #endif //PIG_CORE_IMPLEMENTATION
