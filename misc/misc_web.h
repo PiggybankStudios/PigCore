b3873b21c9c1b5190dd22b6578377341311cc1ca
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2025-08-16 21:31:35 -0700

    Added GetHttpStatusCodeDescription to misc_web.h with a fair number of entries for accepted HTTP status codes

--- misc/misc_web.h
+++ misc/misc_web.h
@@ -103,6 +103,7 @@ PEXP const char* GetMimeTypeOfficialName(MimeType enumValue)
 	Str8 EscapeStr_FormUrlEncoding(Arena* arena, Str8 str, bool addNullTerm);
 	Str8 EncodeHttpKeyValuePairContent(Arena* arena, uxx numItems, const Str8Pair* contentItems, MimeType encoding, bool addNullTerm);
 	uxx DecodeHttpHeaders(Arena* arena, Str8 encodedHeadersStr, bool allocatePairSlices, Str8Pair** headersOut);
+	const char* GetHttpStatusCodeDescription(u16 code);
 #endif
 
 // +--------------------------------------------------------------+
@@ -461,6 +462,100 @@ PEXP uxx DecodeHttpHeaders(Arena* arena, Str8 encodedHeadersStr, bool allocatePa
 	Assert(headerIndex == numHeaders);
 	SetOptionalOutPntr(headersOut, results);
 	return numHeaders;
+}
+
+//See https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+PEXP const char* GetHttpStatusCodeDescription(u16 code)
+{
+	switch (code)
+	{
+		case 100: return "Continue";
+		case 101: return "Switching Protocols";
+		case 102: return "Processing";
+		case 103: return "Early Hints";
+		
+		case 200: return "OK";
+		case 201: return "Created";
+		case 202: return "Accepted";
+		case 203: return "Non-Authoritative Information";
+		case 204: return "No Content";
+		case 205: return "Reset Content";
+		case 206: return "Partial Content";
+		case 207: return "Multi-Status";
+		case 208: return "Already Reported";
+		case 211: return "This is fine"; //unofficial
+		case 226: return "IM Used";
+		
+		case 300: return "Multiple Choices";
+		case 301: return "Moved Permanently";
+		case 302: return "Found";
+		case 303: return "See Other";
+		case 304: return "Not Modified";
+		case 305: return "Use Proxy";
+		case 306: return "Switch Proxy";
+		case 307: return "Temporary Redirect";
+		case 308: return "Permanent Redirect";
+		
+		case 400: return "Bad Request";
+		case 401: return "Unauthorized";
+		case 402: return "Payment Required";
+		case 403: return "Forbidden";
+		case 404: return "Not Found";
+		case 405: return "Method Not Allowed";
+		case 406: return "Not Acceptable";
+		case 407: return "Proxy Authentication Required";
+		case 408: return "Request Timeout";
+		case 409: return "Conflict";
+		case 410: return "Gone";
+		case 411: return "Length Required";
+		case 412: return "Precondition Failed";
+		case 413: return "Payload Too Large";
+		case 414: return "URI Too Long";
+		case 415: return "Unsupported Media Type";
+		case 416: return "Range Not Satisfiable";
+		case 417: return "Expectation Failed";
+		case 418: return "I'm a teapot";
+		case 419: return "Page Expired"; //unofficial
+		case 420: return "Method Failure/Enhance Your Calm"; //unofficial
+		case 421: return "Misdirected Request";
+		case 422: return "Unprocessable Content";
+		case 423: return "Locked";
+		case 424: return "Failed Dependency";
+		case 425: return "Too Early";
+		case 426: return "Upgrade Required";
+		case 428: return "Precondition Required";
+		case 429: return "Too Many Requests";
+		case 430: return "Shopify Security Rejection"; //unofficial
+		case 431: return "Request Header Fields Too Large";
+		case 450: return "Blocked by Windows Parental Controls"; //unofficial
+		case 451: return "Unavailable For Legal Reasons";
+		case 498: return "Invalid Token"; //unofficial
+		case 499: return "Token Required"; //unofficial
+		
+		case 500: return "Internal Server Error";
+		case 501: return "Not Implemented";
+		case 502: return "Bad Gateway";
+		case 503: return "Service Unavailable";
+		case 504: return "Gateway Timeout";
+		case 505: return "HTTP Version Not Supported";
+		case 506: return "Variant Also Negotiates";
+		case 507: return "Insufficient Storage";
+		case 508: return "Loop Detected";
+		case 509: return "Bandwidth Limit Exceeded"; //unofficial
+		case 510: return "Not Extended";
+		case 511: return "Network Authentication Required";
+		case 529: return "Site is overloaded"; //unofficial
+		case 530: return "Site is frozen/Origin DNS Error"; //unofficial
+		case 540: return "Temporarily Disabled"; //unofficial
+		case 598: return "Network read timeout error"; //unofficial
+		case 599: return "Network Connect Timeout Error"; //unofficial
+		
+		case 783: return "Unexpected Token"; //unofficial
+		
+		case 999: return "Non-standard"; //unofficial
+		
+		default: return nullptr;
+	}
 }
 
 #endif //PIG_CORE_IMPLEMENTATION
