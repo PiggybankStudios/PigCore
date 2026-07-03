10958a86a62a583dd7eefff7592d196d9a119848
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-07-03 14:04:17 -0700

    Finished conversion of struct_quaternion.h

--- src/struct/struct_quaternion.h
+++ src/struct/struct_quaternion.h
@@ -28,19 +28,15 @@ Description:
 typedef car Quaternion_R32 Quaternion_R32;
 car Quaternion_R32
 {
-	float elem[4];
-	plex
-	{
-		car
-		{
-			v3 xyz;
-			plex { float x, y, z; };
-		};
-		float w;
-	};
+	r32 elem[4];
+	plex { r32 x, y, z, w; };
+	plex { v3 xyz; r32 _unused1; };
 	// #ifdef HANDMADE_MATH__USE_SSE
 	// __m128 SSE;
 	// #endif
+	#if LANGUAGE_IS_CPP
+	inline r32& operator[](const int& elemIndex) { return elem[elemIndex]; }
+	#endif
 };
 #define MakeQuat_Const(X, Y, Z, W) { .x=(X), .y=(Y), .z=(Z), .w=(W) }
 #define MakeQuat(x, y, z, w)       NEW_STRUCT(Quaternion_R32)MakeQuat_Const((x), (y), (z), (w))
@@ -49,15 +45,11 @@ typedef car Quaternion_R64 Quaternion_R64;
 car Quaternion_R64
 {
 	r64 elem[4];
-	plex
-	{
-		car
-		{
-			v3d xyz;
-			plex { r64 x, y, z; };
-		};
-		r64 w;
-	};
+	plex { r64 x, y, z, w; };
+	plex { v3d xyz; r64 _unused1; };
+	#if LANGUAGE_IS_CPP
+	inline r64& operator[](const int& elemIndex) { return elem[elemIndex]; }
+	#endif
 };
 #define MakeQuatd_Const(X, Y, Z, W) { .x=(X), .y=(Y), .z=(Z), .w=(W) }
 #define MakeQuatd(x, y, z, w)       NEW_STRUCT(Quaternion_R64)MakeQuatd_Const((x), (y), (z), (w))
@@ -69,6 +61,26 @@ typedef Quaternion_R64 quatd;
 // |                 Header Function Declarations                 |
 // +--------------------------------------------------------------+
 #if !PIG_CORE_IMPLEMENTATION
+	PIG_CORE_INLINE quat ToQuatFromV4(v4 vec4);
+	PIG_CORE_INLINE quatd ToQuatdFromV4d(v4d vec4d);
+	PIG_CORE_INLINE v4 ToV4FromQuat(quat quaternion);
+	PIG_CORE_INLINE v4d ToV4dFromQuatd(quatd quaternion);
+	PIG_CORE_INLINE quat ToQuatFromAxis_RH(v3 axisVec, r64 angleOfRotation);
+	PIG_CORE_INLINE bool AreEqualQuat(quat left, quat right);
+	PIG_CORE_INLINE bool AreEqualQuatd(quatd left, quatd right);
+	PIG_CORE_INLINE quat AddQuat(quat left, quat right);
+	PIG_CORE_INLINE quat SubQuat(quat left, quat right);
+	PIG_CORE_INLINE quat MulQuat(quat left, quat right);
+	PIG_CORE_INLINE quat ScaleQuat(quat left, r32 scalar);
+	PIG_CORE_INLINE quat ShrinkQuat(quat left, r32 scalar);
+	PIG_CORE_INLINE r32 DotQuat(quat left, quat right);
+	PIG_CORE_INLINE quat InvertQuat(quat left);
+	PIG_CORE_INLINE r32 LengthSquaredQuat(v2 quaternion);
+	PIG_CORE_INLINE r32 LengthQuat(v2 quaternion);
+	PIG_CORE_INLINE quat NormalizeQuat(quat quaternion);
+	PIG_CORE_INLINE quat MixQuat(quat left, float mixLeft, quat right, float mixRight);
+	PIG_CORE_INLINE quat LerpQuat(quat start, quat right, r32 amount);
+	PIG_CORE_INLINE quat SlerpQuat(quat start, quat right, r32 amount);
 	PIG_CORE_INLINE quatd ToQuatdFromAxis(v3d axis, r64 angle);
 	PIG_CORE_INLINE quatd AddQuatd(quatd left, quatd right);
 	PIG_CORE_INLINE quatd SubQuatd(quatd left, quatd right);
@@ -80,8 +92,6 @@ typedef Quaternion_R64 quatd;
 	PIG_CORE_INLINE quatd NormalizeQuatd(quatd quaternion);
 	PIG_CORE_INLINE quatd LerpQuatd(quatd start, quatd end, r64 amount);
 	PIG_CORE_INLINE quatd SlerpQuatd(quatd start, quatd end, r64 amount);
-	PIG_CORE_INLINE bool AreEqualQuat(quat left, quat right);
-	PIG_CORE_INLINE bool AreEqualQuatd(quatd left, quatd right);
 #endif //!PIG_CORE_IMPLEMENTATION
 
 // +--------------------------------------------------------------+
@@ -113,7 +123,7 @@ PEXPI quatd ToQuatdFromV4d(v4d vec4d) { return MakeQuatd(vec4d.x, vec4d.y, vec4d
 PEXPI v4 ToV4FromQuat(quat quaternion) { return MakeV4(quaternion.x, quaternion.y, quaternion.z, quaternion.w); }
 PEXPI v4d ToV4dFromQuatd(quatd quaternion) { return MakeV4d(quaternion.x, quaternion.y, quaternion.z, quaternion.w); }
 
-PEXPI quat ToQuatFromAxis_RH(v3 axisVec, r32 angleOfRotation)
+PEXPI quat ToQuatFromAxis_RH(v3 axisVec, r64 angleOfRotation)
 {
 	quat result;
 	v3 axisNormalized = NormalizeV3(axisVec);
@@ -123,32 +133,35 @@ PEXPI quat ToQuatFromAxis_RH(v3 axisVec, r32 angleOfRotation)
 	return result;
 }
 
+PEXPI bool AreEqualQuat(quat left, quat right) { return (left.x == right.x && left.y == right.y && left.z == right.z && left.w == right.w); }
+PEXPI bool AreEqualQuatd(quatd left, quatd right) { return (left.x == right.x && left.y == right.y && left.z == right.z && left.w == right.w); }
+
 PEXPI quat AddQuat(quat left, quat right)
 {
-    quat result;
+	quat result;
 	// #ifdef HANDMADE_MATH__USE_SSE
-    // result.SSE = _mm_add_ps(left.SSE, right.SSE);
+	// result.SSE = _mm_add_ps(left.SSE, right.SSE);
 	// #else
-    result.x = left.x + right.x;
-    result.y = left.y + right.y;
-    result.z = left.z + right.z;
-    result.w = left.w + right.w;
+	result.x = left.x + right.x;
+	result.y = left.y + right.y;
+	result.z = left.z + right.z;
+	result.w = left.w + right.w;
 	// #endif
-    return result;
+	return result;
 }
 
 PEXPI quat SubQuat(quat left, quat right)
 {
-    quat result;
+	quat result;
 	// #ifdef HANDMADE_MATH__USE_SSE
-    // result.SSE = _mm_sub_ps(left.SSE, right.SSE);
+	// result.SSE = _mm_sub_ps(left.SSE, right.SSE);
 	// #else
-    result.x = left.x - right.x;
-    result.y = left.y - right.y;
-    result.z = left.z - right.z;
-    result.w = left.w - right.w;
+	result.x = left.x - right.x;
+	result.y = left.y - right.y;
+	result.z = left.z - right.z;
+	result.w = left.w - right.w;
 	// #endif
-    return result;
+	return result;
 }
 
 PEXPI quat MulQuat(quat left, quat right)
@@ -158,88 +171,155 @@ PEXPI quat MulQuat(quat left, quat right)
 	// __m128 SSEResultOne = _mm_xor_ps(_mm_shuffle_ps(left.SSE, left.SSE, _MM_SHUFFLE(0, 0, 0, 0)), _mm_setr_ps(0.f, -0.f, 0.f, -0.f));
 	// __m128 SSEResultTwo = _mm_shuffle_ps(right.SSE, right.SSE, _MM_SHUFFLE(0, 1, 2, 3));
 	// __m128 SSEResultThree = _mm_mul_ps(SSEResultTwo, SSEResultOne);
-	
 	// SSEResultOne = _mm_xor_ps(_mm_shuffle_ps(left.SSE, left.SSE, _MM_SHUFFLE(1, 1, 1, 1)) , _mm_setr_ps(0.f, 0.f, -0.f, -0.f));
 	// SSEResultTwo = _mm_shuffle_ps(right.SSE, right.SSE, _MM_SHUFFLE(1, 0, 3, 2));
 	// SSEResultThree = _mm_add_ps(SSEResultThree, _mm_mul_ps(SSEResultTwo, SSEResultOne));
-	
 	// SSEResultOne = _mm_xor_ps(_mm_shuffle_ps(left.SSE, left.SSE, _MM_SHUFFLE(2, 2, 2, 2)), _mm_setr_ps(-0.f, 0.f, 0.f, -0.f));
 	// SSEResultTwo = _mm_shuffle_ps(right.SSE, right.SSE, _MM_SHUFFLE(2, 3, 0, 1));
 	// SSEResultThree = _mm_add_ps(SSEResultThree, _mm_mul_ps(SSEResultTwo, SSEResultOne));
-	
 	// SSEResultOne = _mm_shuffle_ps(left.SSE, left.SSE, _MM_SHUFFLE(3, 3, 3, 3));
 	// SSEResultTwo = _mm_shuffle_ps(right.SSE, right.SSE, _MM_SHUFFLE(3, 2, 1, 0));
 	// result.SSE = _mm_add_ps(SSEResultThree, _mm_mul_ps(SSEResultTwo, SSEResultOne));
 	// #else
-	result.x =  right.Elements[3] * +left.Elements[0];
-	result.y =  right.Elements[2] * -left.Elements[0];
-	result.z =  right.Elements[1] * +left.Elements[0];
-	result.w =  right.Elements[0] * -left.Elements[0];
+	result.x =  right.elem[3] * +left.elem[0];
+	result.y =  right.elem[2] * -left.elem[0];
+	result.z =  right.elem[1] * +left.elem[0];
+	result.w =  right.elem[0] * -left.elem[0];
 	
-	result.x += right.Elements[2] * +left.Elements[1];
-	result.y += right.Elements[3] * +left.Elements[1];
-	result.z += right.Elements[0] * -left.Elements[1];
-	result.w += right.Elements[1] * -left.Elements[1];
+	result.x += right.elem[2] * +left.elem[1];
+	result.y += right.elem[3] * +left.elem[1];
+	result.z += right.elem[0] * -left.elem[1];
+	result.w += right.elem[1] * -left.elem[1];
 	
-	result.x += right.Elements[1] * -left.Elements[2];
-	result.y += right.Elements[0] * +left.Elements[2];
-	result.z += right.Elements[3] * +left.Elements[2];
-	result.w += right.Elements[2] * -left.Elements[2];
+	result.x += right.elem[1] * -left.elem[2];
+	result.y += right.elem[0] * +left.elem[2];
+	result.z += right.elem[3] * +left.elem[2];
+	result.w += right.elem[2] * -left.elem[2];
 	
-	result.x += right.Elements[0] * +left.Elements[3];
-	result.y += right.Elements[1] * +left.Elements[3];
-	result.z += right.Elements[2] * +left.Elements[3];
-	result.w += right.Elements[3] * +left.Elements[3];
+	result.x += right.elem[0] * +left.elem[3];
+	result.y += right.elem[1] * +left.elem[3];
+	result.z += right.elem[2] * +left.elem[3];
+	result.w += right.elem[3] * +left.elem[3];
 	// #endif
 	return result;
 }
 
-PEXPI quat HMM_MulQF(quat left, float Multiplicative)
+PEXPI quat ScaleQuat(quat left, r32 scalar)
 {
 	quat result;
 	// #ifdef HANDMADE_MATH__USE_SSE
-	// __m128 Scalar = _mm_set1_ps(Multiplicative);
-	// result.SSE = _mm_mul_ps(left.SSE, Scalar);
+	// __m128 scalar = _mm_set1_ps(scalar);
+	// result.SSE = _mm_mul_ps(left.SSE, scalar);
 	// #else
-	result.x = left.x * Multiplicative;
-	result.y = left.y * Multiplicative;
-	result.z = left.z * Multiplicative;
-	result.w = left.w * Multiplicative;
+	result.x = left.x * scalar;
+	result.y = left.y * scalar;
+	result.z = left.z * scalar;
+	result.w = left.w * scalar;
 	// #endif
 	return result;
 }
 
-PEXPI quat HMM_DivQF(quat left, float Divnd)
+PEXPI quat ShrinkQuat(quat left, r32 scalar)
 {
-    quat result;
+	quat result;
+	// #ifdef HANDMADE_MATH__USE_SSE
+	// __m128 Scalar = _mm_set1_ps(scalar);
+	// result.SSE = _mm_div_ps(left.SSE, Scalar);
+	// #else
+	result.x = left.x / scalar;
+	result.y = left.y / scalar;
+	result.z = left.z / scalar;
+	result.w = left.w / scalar;
+	// #endif
+	return result;
+}
 
-#ifdef HANDMADE_MATH__USE_SSE
-    __m128 Scalar = _mm_set1_ps(Divnd);
-    result.SSE = _mm_div_ps(left.SSE, Scalar);
-#else
-    result.x = left.x / Divnd;
-    result.y = left.y / Divnd;
-    result.z = left.z / Divnd;
-    result.w = left.w / Divnd;
-#endif
+PEXPI r32 DotQuat(quat left, quat right)
+{
+	r32 result;
+	// #ifdef HANDMADE_MATH__USE_SSE
+	// __m128 SSEResultOne = _mm_mul_ps(left.SSE, right.SSE);
+	// __m128 SSEResultTwo = _mm_shuffle_ps(SSEResultOne, SSEResultOne, _MM_SHUFFLE(2, 3, 0, 1));
+	// SSEResultOne = _mm_add_ps(SSEResultOne, SSEResultTwo);
+	// SSEResultTwo = _mm_shuffle_ps(SSEResultOne, SSEResultOne, _MM_SHUFFLE(0, 1, 2, 3));
+	// SSEResultOne = _mm_add_ps(SSEResultOne, SSEResultTwo);
+	// _mm_store_ss(&result, SSEResultOne);
+	// #else
+	result = ((left.x * right.x) + (left.z * right.z)) + ((left.y * right.y) + (left.w * right.w));
+	// #endif
+	return result;
+}
+PEXPI quat InvertQuat(quat left)
+{
+	quat result;
+	result.x = -left.x;
+	result.y = -left.y;
+	result.z = -left.z;
+	result.w = left.w;
+	return ShrinkQuat(result, DotQuat(left, left));
+}
 
+PEXPI r32 LengthSquaredQuat(v2 quaternion)
+{
+	return DotQuat(quaternion, quaternion);
+}
+PEXPI r32 LengthQuat(v2 quaternion)
+{
+	return SqrtR32(LengthSquaredQuat(quaternion));
+}
+
+PEXPI quat NormalizeQuat(quat quaternion)
+{
+	return  ShrinkQuat(quaternion, LengthQuat(quaternion));
+}
+
+PEXPI quat MixQuat(quat left, float mixLeft, quat right, float mixRight) {
+	quat result;
+	// #ifdef HANDMADE_MATH__USE_SSE
+	// __m128 scalarLeft = _mm_set1_ps(mixLeft);
+	// __m128 scalarRight = _mm_set1_ps(mixRight);
+	// __m128 SSEResultOne = _mm_mul_ps(left.SSE, scalarLeft);
+	// __m128 SSEResultTwo = _mm_mul_ps(right.SSE, scalarRight);
+	// result.SSE = _mm_add_ps(SSEResultOne, SSEResultTwo);
+	// #else
+	result.x = left.x*mixLeft + right.x*mixRight;
+	result.y = left.y*mixLeft + right.y*mixRight;
+	result.z = left.z*mixLeft + right.z*mixRight;
+	result.w = left.w*mixLeft + right.w*mixRight;
+	// #endif
     return result;
 }
 
-//TODO: #define AddQuat(left, right) HMM_AddQ((left), (right))
-//TODO: #define SubQuat(left, right) HMM_SubQ((left), (right))
-//TODO: #define MulQuat(left, right) HMM_MulQ((left), (right))
-//TODO: #define ScaleQuat(quaternion, scalar) HMM_MulQF((quaternion), (scalar))
-//TODO: #define ShrinkQuat(quaternion, divisor) HMM_DivQF((quaternion), (divisor))
-
-//TODO: #define DotQuat(left, right) HMM_DotQ((left), (right))
-
-//TODO: #define InvertQuat(quaternion) HMM_InvQ(quaternion)
-
-//TODO: #define NormalizeQuat(quaternion) HMM_NormQ(quaternion)
-
-//TODO: #define LerpQuat(start, end, amount) HMM_NLerp((start), (amount), (end))
-//TODO: #define SlerpQuat(start, end, amount) HMM_SLerp((start), (amount), (end))
+PEXPI quat LerpQuat(quat start, quat right, r32 amount)
+{
+    quat result = MixQuat(start, 1.0f - amount, right, amount);
+    result = NormalizeQuat(result);
+    return result;
+}
+PEXPI quat SlerpQuat(quat start, quat right, r32 amount)
+{
+	quat result;
+	r32 cosTheta = DotQuat(start, right);
+	if (cosTheta < 0.0f) // NOTE(lcf): Take shortest path on Hyper-sphere
+	{
+		cosTheta = -cosTheta;
+		right = MakeQuat(-right.X, -right.Y, -right.Z, -right.W);
+	}
+	// NOTE(lcf): Use Normalized Linear interpolation when vectors are roughly not L.I.
+	if (cosTheta > 0.9995f)
+	{
+		result = LerpQuat(start, amount, right);
+	}
+	else
+	{
+		r32 angle = AcosR32(cosTheta);
+		r32 mixLeft = SinR32((1.0f - amount) * angle);
+		r32 mixRight = SinR32(amount * angle);
+		result = MixQuat(start, mixLeft, right, mixRight);
+		result = NormalizeQuat(result);
+	}
+	return result;
+}
 
 PEXPI quatd ToQuatdFromAxis(v3d axis, r64 angle)
 {
@@ -303,7 +383,7 @@ PEXPI quatd ShrinkQuatd(quatd quaternion, r64 divisor)
 
 PEXPI r64 DotQuatd(quatd left, quatd right)
 {
-	//NOTE: We removed "extra" parenthesis around these adds, but technically this might cause slightly more floating point error accumulation
+	//NOTE: We removed "extra" parenthesis around these adds, but technically this might cause slightly more r32ing point error accumulation
 	return (left.x * right.x) + (left.y * right.y) + (left.z * right.z) + (left.w * right.w);
 }
 
@@ -341,7 +421,7 @@ PEXPI quatd SlerpQuatd(quatd start, quatd end, r64 amount)
 		end = MakeQuatd(-end.x, -end.y, -end.z, -end.w);
 	}
 	
-	if (cosTheta > 0.9995) //TODO: Should we choose a number closer to 1.0 when working in 64-bit floats?
+	if (cosTheta > 0.9995) //TODO: Should we choose a number closer to 1.0 when working in 64-bit r32s?
 	{
 		result = LerpQuatd(start, end, amount);
 	}
@@ -356,9 +436,6 @@ PEXPI quatd SlerpQuatd(quatd start, quatd end, r64 amount)
 	
 	return result;
 }
-
-PEXPI bool AreEqualQuat(quat left, quat right) { return (left.x == right.x && left.y == right.y && left.z == right.z && left.w == right.w); }
-PEXPI bool AreEqualQuatd(quatd left, quatd right) { return (left.x == right.x && left.y == right.y && left.z == right.z && left.w == right.w); }
 
 #endif //PIG_CORE_IMPLEMENTATION
 
