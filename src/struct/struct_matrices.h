c76afa126e23271c6ef164ddcd239fc1ea87a665
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-07-05 11:35:15 -0700

    Fixed one more scaling factor of 0.0f instead of 1.0f in MakeScaleXYMat4

--- src/struct/struct_matrices.h
+++ src/struct/struct_matrices.h
@@ -259,7 +259,7 @@ typedef Matrix4x4_R32 mat4;
 #define MakeScaleMat4_Const(scaleVec) MakeScaleXYZMat4_Const((scaleVec).x, (scaleVec).y, (scaleVec).z)
 #define MakeScaleXYZMat4(x, y, z)     NEW_STRUCT(Matrix4x4_R32)MakeScaleXYZMat4_Const((x), (y), (z))
 #define MakeScaleMat4(scaleVec)       NEW_STRUCT(Matrix4x4_R32)MakeScaleXYZMat4_Const((scaleVec).x, (scaleVec).y, (scaleVec).z)
-#define MakeScaleXYMat4(x, y)         MakeScaleXYZMat4((x), (y), 0.0f)
+#define MakeScaleXYMat4(x, y)         MakeScaleXYZMat4((x), (y), 1.0f)
 #define Make2DScaleMat4(scaleVec)     MakeScaleXYMat4((scaleVec).x, (scaleVec).y)
 #define MakeScaleXMat4(x)             MakeScaleXYZMat4((x),  1.0f, 1.0f)
 #define MakeScaleYMat4(y)             MakeScaleXYZMat4(1.0f,  (y), 1.0f)
