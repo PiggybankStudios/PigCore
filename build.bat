9224045ab5a5b2e7d681d1ef627be21ecc74c7a9
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-08-01 18:06:54 -0700

    Added support for automatically cloning pig_build if it doesn't exist at the expected location. This is logic that has to live in the build.bat/sh in the root directory of each project that depends on PigBuild so we may want to find a cleaner/simpler way to do this but it's fine for now

--- build.bat
+++ build.bat
@@ -1,2 +1,15 @@
 @echo off
-call ..\pig_build\shell\build.bat %*
+
+setlocal enabledelayedexpansion
+set PIG_BUILD_RELATIVE_PATH=..\pig_build
+for %%I in ("%PIG_BUILD_RELATIVE_PATH%") do set "PIG_BUILD_ABSOLUTE_PATH=%%~fI"
+if not exist "%PIG_BUILD_RELATIVE_PATH%" (
+	git --version > NUL 2> NUL
+	if NOT "!ERRORLEVEL!"=="0" (
+		echo "Git is not installed. Please manually download PigBuild into %PIG_BUILD_ABSOLUTE_PATH%"
+		exit
+	)
+	git clone https://github.com/PiggybankStudios/PigBuild %PIG_BUILD_RELATIVE_PATH%
+)
+
+call %PIG_BUILD_RELATIVE_PATH%\shell\build.bat %*
--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# TODO: We should pull out PIG_BUILD_RELATIVE_PATH and PIG_BUILD_ABSOLUTE_PATH like the batch script
+if [ ! -d "../pig_build" ]; then
+	if [ ! which git ]; then
+		echo "Git is not installed! Please download PigBuild into $(cd "../pig_build" && pwd)"
+	fi
+	git clone https://github.com/PiggybankStudios/PigBuild ../pig_build
+fi
+
 ../pig_build/shell/build.sh $@
